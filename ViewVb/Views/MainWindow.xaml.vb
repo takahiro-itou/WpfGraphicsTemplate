aa0c2dfcdd790b55e9f79124c9a629b10ca49130
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-30 22:34:36 +0900

    画像の描画処理を仮実装

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -14,6 +14,67 @@ Public Sub New()
 End Sub
 
 
+Private Sub runCommand()
+''--------------------------------------------------------------------
+''    指定したコマンドを実行する。
+''--------------------------------------------------------------------
+Dim imgCanvas As System.Drawing.Bitmap
+Dim grpCanvas As System.Drawing.Graphics
+Dim imgBuffer As System.Drawing.Bitmap
+Dim grpBuffer As System.Drawing.Graphics
+Dim hDisplayDC As IntPtr
+Dim hDC As IntPtr
+Dim brushBG As System.Drawing.SolidBrush
+Dim colorBG As System.Drawing.Color
+Dim hBitmap As IntPtr
+Dim bmpSrc As System.Windows.Media.Imaging.BitmapSource
+
+    hDisplayDC = GetDC(IntPtr.Zero)
+
+    imgBuffer = New System.Drawing.Bitmap(200, 100)
+    grpBuffer = System.Drawing.Graphics.FromImage(imgBuffer)
+
+    colorBG = System.Drawing.Color.FromArgb(&HFFFEF0BA)
+    brushBG = New System.Drawing.SolidBrush(colorBG)
+    ' grpBuffer.FillRectangle(brushBG, grpBuffer.VisibleClipBounds)
+    grpBuffer.FillRectangle(brushBG, 0, 0, 200, 100)
+
+    hDC = grpBuffer.GetHdc()
+    BitBlt(hDC, 8, 8, 184, 84, hDisplayDC,
+            SystemParameters.PrimaryScreenWidth - 184,
+            SystemParameters.PrimaryScreenHeight - 84,
+            SRCCOPY)
+    grpBuffer.ReleaseHdc(hDC)
+
+    grpBuffer.DrawRectangle(System.Drawing.Pens.Yellow, 50, 30, 100, 60)
+    grpBuffer.DrawPie(System.Drawing.Pens.Red, 60, 10, 80, 80, 30, 300)
+    grpBuffer.Dispose()
+
+    imgCanvas = New System.Drawing.Bitmap(300, 300)
+    grpCanvas = System.Drawing.Graphics.FromImage(imgCanvas)
+
+    colorBG = System.Drawing.Color.FromArgb(&H800000FF)
+    brushBG = New System.Drawing.SolidBrush(colorBG)
+    ' grpCanvas.FillRectangle(brushBG, grpCanvas.VisibleClipBounds)
+    grpCanvas.FillRectangle(brushBG, 0, 0, 300, 300)
+
+    hDC = grpCanvas.GetHdc()
+    BitBlt(hDC, 8, 8, 284, 284, hDisplayDC, 0, 0, SRCCOPY)
+    grpCanvas.ReleaseHdc(hDC)
+
+    ReleaseDC(IntPtr.Zero, hDisplayDC)
+
+    grpCanvas.DrawImage(imgBuffer, 50, 100, 200, 100)
+    grpCanvas.Dispose()
+
+    hBitmap = imgCanvas.GetHbitmap()
+    bmpSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+            hBitmap, IntPtr.Zero, Int32Rect.Empty,
+            BitmapSizeOptions.FromEmptyOptions())
+    picView.Source = bmpSrc
+End Sub
+
+
 Private Sub mnuFileExit_Click(ByVal sender As Object, ByVal e As EventArgs)
 ''--------------------------------------------------------------------
 ''    メニュー「ファイル」－「終了」
---
+++ ViewVb/WinAPI.vb
@@ -0,0 +1,41 @@
+﻿
+Imports System.Runtime.InteropServices
+
+Module WinAPI
+
+Public Const SRCAND As Integer = &H8800C6
+Public Const SRCCOPY As Integer = &HCC0020
+Public Const SRCPAINT As Integer = &HEE0086
+
+<DllImport("gdi32.dll")> _
+Public Function BitBlt(ByVal hDestDC As IntPtr, _
+    ByVal X As Integer, ByVal Y As Integer, _
+    ByVal nWidth As Integer, ByVal nHeight As Integer, _
+    ByVal hSrcDC As IntPtr, _
+    ByVal xSrc As Integer, ByVal ySrc As Integer, _
+    ByVal dwRop As Integer) As Integer
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Function CreateCompatibleDC(ByVal hDC As IntPtr) As IntPtr
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Function DeleteDC(ByVal hDC As IntPtr) As Integer
+End Function
+
+<DllImport("user32.dll")> _
+Public Function GetDC(ByVal hWnd As IntPtr) As IntPtr
+End Function
+
+<DllImport("user32.dll")> _
+Public Function ReleaseDC(ByVal hWnd As IntPtr, _
+    ByVal hDC As IntPtr) As IntPtr
+End Function
+
+<DllImport("gdi32.dll")> _
+Public Function SelectObject( _
+    ByVal hDC As IntPtr, ByVal hGdiObj As IntPtr) As IntPtr
+End Function
+
+End Module
