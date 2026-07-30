fc007fe1c8b6abfc02e6a8c43c7e7f86580e5fe7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-30 12:32:49 +0900

    コンパイルエラーを修正

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -4,14 +4,14 @@ Namespace Global.ViewVb.Views
 
 Public Class MainWindow
 
-Private m_model As MySampleModel
+Private m_model As Models.MySampleModel
 
 Public Sub New()
 ''--------------------------------------------------------------------
 ''    コンストラクタ
 ''--------------------------------------------------------------------
     InitializeComponent()
-    Me.m_model = New MySampleModel()
+    Me.m_model = New Models.MySampleModel()
     SampleControl1.ViewModel = New WpfControl.Sample.SampleViewModel(m_model)
 End Sub
 
