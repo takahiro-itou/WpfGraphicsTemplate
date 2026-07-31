a503e3a98e84950bebbbc2137368b2f78b367817
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-31 12:27:11 +0900

    別のテンプレートリポジトリからファイルをコピー

---
+++ Wrapper/Images/FullColorImage.cpp
@@ -0,0 +1,161 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Graphics Test Project.   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of FullColorImage class.
+**
+**      @file       Images/FullColorImage.cpp
+**/
+
+#include "StdAfx.h"
+
+#include "FullColorImage.h"
+
+
+namespace  SampleWrapper  {
+namespace  Images  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace
+
+
+//========================================================================
+//
+//    FullColorImage  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+FullColorImage::FullColorImage()
+    : m_ptrObj(new WrapTarget())
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+FullColorImage::~FullColorImage()
+{
+    //  マネージドリソースを破棄する。              //
+
+    //  続いて、アンマネージドリソースも破棄する。  //
+    this->!FullColorImage();
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （ファイナライザ）。
+//
+
+FullColorImage::!FullColorImage()
+{
+    delete  this->m_ptrObj;
+    this->m_ptrObj  = nullptr;
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//----------------------------------------------------------------
+//    イメージを作成する。
+//
+
+void
+FullColorImage::createImage(
+        const  int  nWidth,
+        const  int  nHeight,
+        const  int  cbPixel,
+        const  int  lStride,
+        void  *     lpBits)
+{
+    return  this->m_ptrObj->createImage(
+                nWidth, nHeight, cbPixel, lStride, lpBits
+    );
+}
+
+//----------------------------------------------------------------
+//    サンプル画像を描画する。
+//
+
+void
+FullColorImage::drawSample()
+{
+    return  this->m_ptrObj->drawSample();
+}
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//----------------------------------------------------------------
+//    矩形を描画する。
+//
+
+void
+FullColorImage::fillRectangle(
+        const  int  x1,
+        const  int  y1,
+        const  int  x2,
+        const  int  y2,
+        const  int  color)
+{
+    return  this->m_ptrObj->fillRectangle(x1, y1, x2, y2, color);
+}
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+}   //  End of namespace  SampleWrapper
---
+++ Wrapper/Images/FullColorImage.h
@@ -0,0 +1,161 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Graphics Test Project.   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of FullColorImage class.
+**
+**      @file       Images/FullColorImage.h
+**/
+
+#pragma once
+
+#include    "Sample/Images/FullColorImage.h"
+
+
+using namespace System;
+
+namespace  SampleWrapper  {
+namespace  Images  {
+
+//========================================================================
+//
+//    FullColorImage  class.
+//
+
+public ref  class  FullColorImage
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    FullColorImage();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~FullColorImage();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （ファイナライザ）。
+    **
+    **/
+    !FullColorImage();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   イメージを作成する。
+    **
+    **  @param [in] nWidth    イメージの幅
+    **  @param [in] nHeight   イメージの高さ
+    **  @param [in] cbPixel   ピクセル当たりのバイト数。
+    **  @param [in] lStride   行当たりのバイト数。
+    **  @param [in] lpBits    イメージデータ。
+    **/
+    virtual  void
+    createImage(
+            const  int  nWidth,
+            const  int  nHeight,
+            const  int  cbPixel,
+            const  int  lStride,
+            void  *     lpBits);
+
+    //----------------------------------------------------------------
+    /**   サンプル画像を描画する。
+    **
+    **/
+    virtual  void
+    drawSample();
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   矩形を描画する。
+    **
+    **/
+    void
+    fillRectangle(
+            const  int  x1,
+            const  int  y1,
+            const  int  x2,
+            const  int  y2,
+            const  int  color);
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+private:
+
+    typedef     Sample::Images::FullColorImage  WrapTarget;
+
+    WrapTarget  *   m_ptrObj;
+};
+
+}   //  End of namespace  Common
+}   //  End of namespace  SampleWrapper
