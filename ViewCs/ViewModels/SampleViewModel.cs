e819ec64c00ffc9b60215fa073b777fabc3dc4d9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-06 12:37:32 +0900

    リファクタリング
    ビュークラスに記述していた描画処理をビューモデルに移動

--- ViewCs/ViewModels/SampleViewModel.cs
+++ ViewCs/ViewModels/SampleViewModel.cs
@@ -12,10 +12,13 @@
 **                                                                      **
 *************************************************************************/
 
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 using ViewCs.Commands;
@@ -43,7 +46,7 @@ public SampleViewModel()
 
     imgCanvas = new WriteableBitmap(
             300, 300, 96, 96,
-            System.Windows.Media.PixelFormats.Pbgra32, null);
+            PixelFormats.Pbgra32, null);
     m_wrapImage = new SampleWrapper.Images.FullColorImage();
 
     imgCanvas.Lock();
@@ -186,6 +189,26 @@ public  virtual  int
 executeCommand(
         System.IProgress<int>   progress)
 {
+    int     cAlpha;
+    int     colBG, colTL, colTR, colBL, colBR;
+    System.Random   rnd = new System.Random();
+
+    //  色を適当に決める。背景はある程度明るい色
+    cAlpha  = 255 << 24;
+    colBG = rnd.Next(16777216) | cAlpha | 0x00808080;
+
+    //  色を適当に決める。
+    colTL = rnd.Next(256) | cAlpha | 0x00000080;
+    colTR = (rnd.Next(256) <<  8) | cAlpha | 0x00008080;
+    colBL = rnd.Next(256);
+    colBL = (colBL | colBL <<  8) | cAlpha | 0x00008080;
+    colBR = (rnd.Next(256) << 16) | cAlpha | 0x00800000;
+
+    this.m_imgCanvas.Lock();
+    this.m_wrapImg.drawSample(colBG, colTL, colTR, colBL, colBR);
+    this.m_imgCanvas.AddDirtyRect(new Int32Rect(0, 0, 300, 300));
+    this.m_imgCanvas.Unlock();
+
     progress.Report(100);
     return ( 0 );
 }
